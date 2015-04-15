f4ecdfcce35808a0b1d94cf2d23a4f86c7729737
Thien Tran <eva-thien@gmail.com>
2015-04-15 10:51:06 +0700

    add unit test

---
+++ tests/unit-tests/UnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Test;
+
+/**
+ * Class UnitTest
+ */
+class UnitTest extends \PHPUnit_Framework_TestCase {
+
+    public function testTestCase() {
+
+        $this->assertEquals('works',
+            'works',
+            'This is OK'
+        );
+    }
+}
