7fb635649d36d525abba77e20f9818b50596ade5
Serghei Iakovlev <sadhooklay@gmail.com>
2015-08-04 22:42:31 +0300

    Simplifed test bootstrap

--- tests/config/bootstrap.php
+++ tests/config/bootstrap.php
@@ -2,9 +2,9 @@
 
 /*
  +------------------------------------------------------------------------+
- | Phosphorum                                                             |
+ | Phalcon Tip                                                            |
  +------------------------------------------------------------------------+
- | Copyright (c) 2013-2014 Phalcon Team and contributors                  |
+ | Copyright (c) 2013-2015 Phalcon Team and contributors                  |
  +------------------------------------------------------------------------+
  | This source file is subject to the New BSD License that is bundled     |
  | with this package in the file docs/LICENSE.txt.                        |
@@ -18,66 +18,19 @@
 use Phalcon\Http\Response;
 use Phalcon\Mvc\Application;
 use Phalcon\DI\FactoryDefault;
-use Phalcon\Logger\Adapter\File as Logger;
 
-error_reporting(E_ALL);
+defined('APP_PATH') || define('APP_PATH', dirname(dirname(dirname(__FILE__))));
 
-if (!isset($_GET['_url'])) {
-    $_GET['_url'] = '/';
-}
-
-if (! defined('APP_PATH')) {
-    define('APP_PATH', realpath(__DIR__ . '/..'));
-}
-
-/**
- * Read the configuration
- */
-$config = include APP_PATH . "/app/config/config.php";
-
-/**
- * Include the loader
- */
-require APP_PATH . "/app/config/loader.php";
-
-/**
- * Include composer autoloader
- */
 require APP_PATH . "/vendor/autoload.php";
 
-try {
+$config = include APP_PATH . "/app/config/config.php";
+require APP_PATH . "/app/config/loader.php";
 
-    /**
-     * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
-     */
-    $di = new FactoryDefault();
+$di = new FactoryDefault;
 
-    /**
-     * Include the application services
-     */
-    require APP_PATH . "/app/config/services.php";
+require APP_PATH . "/app/config/services.php";
 
-    /**
-     * Handle the request
-     */
-    $application = new Application($di);
+$application = new Application($di);
 
-    return $application;
+return $application;
 
-} catch (Exception $e) {
-
-    /**
-     * Log the exception
-     */
-    $logger = new Logger(APP_PATH . '/app/logs/error.log');
-    $logger->error($e->getMessage());
-    $logger->error($e->getTraceAsString());
-
-    /**
-     * Show an static error page
-     */
-    $response = new Response();
-    $response->redirect('505.html');
-    $response->send();
-
-}
